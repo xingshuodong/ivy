1066a14f2eb07ff829624cf34c0b900bd9e46329
xingshuodong <87846736+xingshuodong@users.noreply.github.com>
2023-09-06 16:15:04 +1000

    Update irfftn

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_fft.py
@@ -225,7 +225,7 @@ def test_paddle_irfft(
 @handle_frontend_test(
     fn_tree="paddle.fft.irfftn",
     dtype_x_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("valid"),
+        available_dtypes=helpers.get_dtypes("complex"),
         min_value=-10,
         max_value=10,
         min_num_dims=1,
@@ -233,32 +233,33 @@ def test_paddle_irfft(
         valid_axis=True,
         force_int_axis=True,
     ),
-    n=st.one_of(
-        st.integers(min_value=2, max_value=10),
-        st.just(None),
-    ),
-    axes=st.lists(st.integers(min_value=0, max_value=10), min_size=1, max_size=3),
     norm=st.sampled_from(["backward", "ortho", "forward"]),
 )
 def test_paddle_irfftn(
     dtype_x_axis,
-    n,
-    axes,
     norm,
     frontend,
-    backend_fw,
     test_flags,
     fn_tree,
+    backend_fw,
 ):
-    input_dtypes, x, _ = dtype_x_axis
+    input_dtypes, x, axis = dtype_x_axis
+
+    # Get the shape of the input array
+    shape = x[0].shape
+
+    # Create a modified shape with only one axis
+    modified_shape = list(shape)
+    modified_shape[axis] = modified_shape[axis] * 2 - 2
+
     helpers.test_frontend_function(
         input_dtypes=input_dtypes,
-        frontend=frontend,
         backend_to_test=backend_fw,
+        frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
-        x=x[0],
-        n=n,
-        axes=axes,
+        x=x,
+        s=modified_shape,
+        axes=[axis],  # Use the single axis value
         norm=norm,
     )
